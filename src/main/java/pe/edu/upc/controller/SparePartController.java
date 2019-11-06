f4d37048600479cb4d485b0b8310f1fb4040de61
OmarAguilar19 <ogalo2661@hotmail.com>
2019-11-06 14:24:02 -0500

    sparepart ready

--- src/main/java/pe/edu/upc/controller/SparePartController.java
+++ src/main/java/pe/edu/upc/controller/SparePartController.java
@@ -1,6 +1,7 @@
 package pe.edu.upc.controller;
 
 import java.util.Map;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -9,16 +10,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import pe.edu.upc.entity.Brand;
 import pe.edu.upc.entity.Sparepart;
 import pe.edu.upc.service.IBrandService;
 import pe.edu.upc.service.ISparePartService;
-
-
 
 @Controller
 @RequestMapping("/spareparts")
@@ -67,21 +69,35 @@ public class SparePartController {
 		}
 		return "/sparepart/listSparePart";
 	}
+
+	@RequestMapping("/update/{id}")
+	public String updateSparePart(@PathVariable int id, Model model, RedirectAttributes objRedir) {
+		Optional<Sparepart> sparepart = sService.listId(id);
+
+		if (sparepart == null) {
+			objRedir.addFlashAttribute("mensaje", "Ocurrio un error");
+			return "redirect:/sparepart/listSparePart";
+		} else {
+			model.addAttribute("sparepart", sparepart);
+			model.addAttribute("listBrands", bService.list());
+			return "/sparepart/sparepart";
+		}
+	}
+
 	@RequestMapping("/delete")
 	public String deleteSparePart(Map<String, Object> model, @RequestParam(value = "id_Sparepart") Integer id) {
 		try {
 			if (id != null && id > 0) {
-				sService.delete(id);
-				model.put("mensaje", "Se eliminó correctamente");
 
+				sService.delete(id);
+				model.put("listSpareParts", sService.list());
 			}
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
-			model.put("mensaje", "No se puede eliminar un producto");
-		}
-		model.put("listProducts", sService.list());
+			model.put("mensaje", "No se puede eliminar");
+			model.put("listSpareParts", sService.list());
 
-//		return "redirect:/categories/list";
+		}
 		return "/sparepart/listSparePart";
 	}
 }

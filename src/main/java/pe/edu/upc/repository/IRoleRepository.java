8331e58ed62fcb41d0f7dab9b431c5565dff7b57
AntonyQP <u20171b627@upc.edu.pe>
2019-11-06 18:56:47 -0500

    # WARNING: head commit changed in the meantime
    Rol y user

--- src/main/java/pe/edu/upc/repository/IRoleRepository.java
+++ src/main/java/pe/edu/upc/repository/IRoleRepository.java
@@ -3,13 +3,10 @@ package pe.edu.upc.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import pe.edu.upc.entity.Role;
 
-@Repository
-public interface IRoleRepository extends JpaRepository<Role, Integer> {
-
-	@Query("select count(b.n_Role) from Role b where b.n_Role=:n_Role")
-	public int findByN_Role(@Param("n_Role") String n_Role);
+public interface IRoleRepository  extends JpaRepository<Role, Integer> {
+	@Query("select count(r.n_Rol) from Role r where r.n_Rol=:n_Rol")
+	public int findByN_Rol(@Param("n_Rol") String n_Rol);
 }

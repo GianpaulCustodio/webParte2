6a8e3007e82e7b55790cd894e1e272f2535ba25d
OmarAguilar19 <ogalo2661@hotmail.com>
2019-11-06 20:09:21 -0500

    inspection controller

--- src/main/java/pe/edu/upc/controller/InspectionController.java
+++ src/main/java/pe/edu/upc/controller/InspectionController.java
@@ -1,36 +1,111 @@
 package pe.edu.upc.controller;
 
+import java.util.Map;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import pe.edu.upc.entity.Inspection;
 import pe.edu.upc.service.IInspectionService;
 import pe.edu.upc.service.IMachineService;
+import pe.edu.upc.service.IUserService;
 
 @Controller
 @RequestMapping("/inspection")
 public class InspectionController {
-
+	
+	@Autowired
+	private IMachineService mService;
+	@Autowired
+	private IUserService uService;
 	@Autowired
 	private IInspectionService iService;
-	
-	@Autowired
-	private IMachineService bService;
-	
-	//private IUserService uService;
-	
-	
+
 	@GetMapping("/new")
 	public String newInspection(Model model) {
-		model.addAttribute("inspection",new Inspection());
-		model.addAttribute("listMachine", bService.list());
-		//model.addAttribute("listUser", uService.list());
+		model.addAttribute("inspection", new Inspection());
+		model.addAttribute("listMachines", mService.list());
+		model.addAttribute("listUsers", uService.list());
 		return "inspection/inspection";
+	}
+
+	@PostMapping("/save")
+	public String saveMachine(@Valid Inspection inspection, BindingResult result, Model model, SessionStatus status)
+			throws Exception {
+		if (result.hasErrors()) {
+			model.addAttribute("listMachines", mService.list());
+			model.addAttribute("listUsers", uService.list());
+			return "inspection/inspection";
+		} else {
+			int rpta = iService.insert(inspection);
+			if (rpta > 0) {
+				model.addAttribute("mensaje", "Ya existe");
+				model.addAttribute("listMachines", mService.list());
+				model.addAttribute("listUsers", uService.list());
+				return "/inspection/inspection";
+			} else {
+				model.addAttribute("mensaje", "Se guardó correctamente");
+				status.setComplete();
+			}
+		}
+		model.addAttribute("listInspections",iService.list());
+
+		return "/inspection/listInspection";
+	}
+
+	@GetMapping("/list")
+	public String listInspection(Model model) {
+		try {
+			model.addAttribute("inspection", new Inspection());
+			model.addAttribute("listInspections", iService.list());
+		} catch (Exception e) {
+			model.addAttribute("error", e.getMessage());
+		}
+		return "/inspection/listInspection";
 	}
 	
 	
+	@RequestMapping("/update/{id}")
+	public String updateMachine(@PathVariable int id, Model model, RedirectAttributes objRedir) {
+		Optional<Inspection> inspection = iService.listId(id);
+
+		if (inspection == null) {
+			objRedir.addFlashAttribute("mensaje", "Ocurrio un error");
+			return "redirect:/inspection/listInspections";
+		} else {
+			model.addAttribute("inspections", inspection);
+			model.addAttribute("listMachines", mService.list());
+			model.addAttribute("listUsers", uService.list());
+			return "/inspection/inspection";
+		}
+	}
 	
+	@RequestMapping("/delete")
+	public String deleteInspection(Map<String, Object> model, @RequestParam(value = "id_Inspection") Integer id) {
+		try {
+			if (id != null && id > 0) {
+				iService.delete(id);
+				model.put("mensaje", "Se eliminó correctamente");
+
+			}
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			model.put("mensaje", "No se puede eliminar el machine");
+		}
+		model.put("listInspections", iService.list());
+		return "/inspection/listInspection";
+	}
 }
+	
